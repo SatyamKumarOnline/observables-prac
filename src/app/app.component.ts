cf1367e1c0a785e09c0f6252a6c1e21a60c21ea3
unknown <registersatyam@gmail.com>
2023-10-19 18:33:34 +0530

    Async pipes :-Removed observables subscription from component.ts and moved it to templates Async pipes help us to subscribe observables in template using async.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,27 +13,13 @@ export class AppComponent {
   public continentDropdown = new FormControl();
   public countryDropDown = new FormControl();
   public countryList: Country[] = [];
-  public currSelectedContinent: String = '';
-  public currSelectedCountry: String = '';
   private endPointUrl: string = '';
 
   constructor(apiServiceService: ApiServiceService) {
-    this.continentDropdown.valueChanges.subscribe((selectedValue) =>
-      this.currSelectedContinent = selectedValue
-    );
-
-    this.countryDropDown.valueChanges.subscribe((countryItem) => {
-      this.currSelectedCountry = countryItem;
-    }
-    );
-
     this.endPointUrl = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-continent.json';
-
     apiServiceService.getItemList(this.endPointUrl).subscribe(
       data => {
-        debugger;
         this.countryList = data;
-        // console.log(data.data);
       }
     );
   }
